d5645486dec340594978f8ccfe7694624bfc6f1c
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2022-10-23 22:00:11 +0200

    Add build script and lint-staged basic prettier and eslint support

--- index.test.ts
+++ index.test.ts
@@ -1,11 +1,11 @@
-import { helloWorld } from "./index";
-import { describe, it } from "node:test";
-import assert from "node:assert";
+import { helloWorld } from './index';
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
 
-describe("Init test", () => {
-  it("runs", () => {
-    const result = helloWorld();
+describe('Init test', () => {
+	it('runs', () => {
+		const result = helloWorld();
 
-    assert.equal(result, "Hello World!");
-  });
+		assert.equal(result, 'Hello World!');
+	});
 });

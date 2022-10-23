7915d5e01e674854dc2a5d0799c3a6ee208a8fc5
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2022-10-23 20:15:21 +0200

    Create basic template

---
+++ index.test.ts
@@ -0,0 +1,11 @@
+import { helloWorld } from "./index";
+import { describe, it } from "node:test";
+import assert from "node:assert";
+
+describe("Init test", () => {
+  it("runs", () => {
+    const result = helloWorld();
+
+    assert.equal(result, "Hello World!");
+  });
+});
